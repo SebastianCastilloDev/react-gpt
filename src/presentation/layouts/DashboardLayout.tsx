00b47bfb2a148ef8efd8978eada26da834216fa6
Sebastián Castillo <tantrum5535@gmail.com>
2025-07-30 00:33:37 -0400

    feat: enhance DashboardLayout with navigation and improved layout

--- src/presentation/layouts/DashboardLayout.tsx
+++ src/presentation/layouts/DashboardLayout.tsx
@@ -1,10 +1,27 @@
-import { Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom";
 
 export const DashboardLayout = () => {
   return (
-    <>
-      <h1 className="fade-in text-3xl font-bold underline">Dashboard</h1>
-      <Outlet />
-    </>
-  )
-}
+    <main className="flex flex-row mt-8 bg-gradient-to-br from-indigo-100 via-white to-indigo-200 min-h-screen">
+      <nav className="hidden sm:flex flex-col ml-8 w-[340px] min-h-[calc(100vh-3.5rem)] bg-white/80 shadow-xl p-7 rounded-2xl border border-indigo-100 backdrop-blur-md">
+        <h1 className="font-extrabold text-2xl lg:text-4xl bg-gradient-to-br from-indigo-700 via-indigo-400 to-indigo-700 bg-clip-text text-transparent drop-shadow-md">
+          ReactGPT<span className="text-indigo-500">.</span>
+        </h1>
+        <span className="text-lg mt-2 text-indigo-700 font-medium">Bienvenido</span>
+
+        <div className="border-b border-indigo-200 my-5" />
+
+        {/* Opciones del menú */}
+
+      </nav>
+
+      <section className="mx-2 sm:mx-12 flex flex-col w-full h-[calc(100vh-60px)] bg-white/90 shadow-lg p-8 rounded-2xl border border-indigo-100">
+        <div className="flex flex-row h-full">
+          <div className="flex flex-col flex-auto h-full p-2">
+            <Outlet />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
